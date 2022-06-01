d497d938e19a6b11d86af3c95f9e5731e9f69aac
AlongkotJ <alongkot.sw@gmail.com>
2022-06-01 17:33:52 +0700

    t

---
+++ todo/todo.go
@@ -0,0 +1,46 @@
+package todo
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+type Todo struct {
+	Title string `json:"text"`
+	gorm.Model
+}
+
+func (Todo) TableName() string {
+	return "todos"
+}
+
+type TodoHandler struct {
+	db *gorm.DB
+}
+
+func NewTodoHandler(db *gorm.DB) *TodoHandler {
+	return &TodoHandler{db: db}
+}
+
+func (t *TodoHandler) NewTask(c *gin.Context) {
+	var todo Todo
+	if err := c.ShouldBindJSON(&todo); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	if err := t.db.Create(todo).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusCreated, gin.H{
+		"id": todo.ID,
+	})
+}
